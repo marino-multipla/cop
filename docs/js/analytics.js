2f229e2db53a7d84f4ea324d5ec37e28d5e89eb3
Giacomo Maccagni <75014126+marino-multipla@users.noreply.github.com>
2024-02-07 16:42:41 +0100

    Update analytics.js

--- docs/js/analytics.js
+++ docs/js/analytics.js
@@ -1,7 +1,9 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J4S8E08B6J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-ga('create', 'G-J4S8E08B6J', 'auto');
-ga('send', 'pageview');
+  gtag('config', 'G-J4S8E08B6J');
+</script>
