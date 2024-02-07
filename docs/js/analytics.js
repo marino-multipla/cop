670ce6b85764e3890866c19f70edb2d6c76dfff2
Giacomo Maccagni <75014126+marino-multipla@users.noreply.github.com>
2024-02-07 16:48:04 +0100

    Update analytics.js

--- docs/js/analytics.js
+++ docs/js/analytics.js
@@ -1,9 +1,6 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-J4S8E08B6J"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// google-analytics.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-J4S8E08B6J');
-</script>
+gtag('config', 'G-J4S8E08B6J');
