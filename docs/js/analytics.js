981c0d9bda64ca168f7f76aadd65882e57e6769c
Giacomo Maccagni <75014126+marino-multipla@users.noreply.github.com>
2024-02-07 16:35:58 +0100

    Update analytics.js

--- docs/js/analytics.js
+++ docs/js/analytics.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-//ga('create', 'XXXXX', 'auto');
-//ga('send', 'pageview');
+ga('create', 'G-J4S8E08B6J', 'auto');
+ga('send', 'pageview');
